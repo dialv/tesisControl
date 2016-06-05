4aaaaad4f4134073d4babf95114bbba2d6143fff
Nancy.Mejia <aty_nan@hotmail.com>
2016-06-05 16:34:04 -0600

    actulizacion en login

--- src/tesisControl/tesisBundle/Controller/DefaultController.php
+++ src/tesisControl/tesisBundle/Controller/DefaultController.php
@@ -15,4 +15,20 @@ class DefaultController extends Controller
     {
         return array();
     }
+    
+    public function loginAction()
+    {
+        $user = 'username';
+        $password = 'password';
+        $em = $this ->getDoctrine()->getEntityManager();
+        $repository = $em ->getRepository('tesisControltesisBundle:Usuario');
+        $user = $repository ->findOneBy(array('id'=>$user,'clave'=>$password));
+        if($user){
+            return $this->render('tesisControltesisBundle:Default:index.html.twig');
+        }else
+        {
+            return $this->render('tesisControltesisBundle:Default:login.html.twig');
+        }
+    }
+   
 }
