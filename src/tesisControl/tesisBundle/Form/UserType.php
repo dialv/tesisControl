cc046eadd803424b6a321438c3e1d064ed216114
victor.alvarado <dialvultmate @gmail.com>
2016-06-13 04:59:22 -0600

    cambios en segurdad y roles

---
+++ src/tesisControl/tesisBundle/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace tesisControl\tesisBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            //->add('salt')
+            ->add('user_roles')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'tesisControl\tesisBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'tesiscontrol_tesisbundle_user';
+    }
+}
